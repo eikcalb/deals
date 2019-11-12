280f0f4e6eeb9b07d8b080b2d18926c5e3f8b8c4
Agwa Israel Onome <agwaisraelonome@gmail.com>
2019-11-12 09:46:01 +0100

    Updates

---
+++ precache-manifest.8ef4ab7ba219589f03b2786aa6331a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc558c07a1e5a9a1f655def607a19b2",
+    "url": "eikcalb.github.io/deals/index.html"
+  },
+  {
+    "revision": "7988173359ab37cbbba5",
+    "url": "eikcalb.github.io/deals/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "503e3e55662a401d0e08",
+    "url": "eikcalb.github.io/deals/static/css/main.b07a680c.chunk.css"
+  },
+  {
+    "revision": "7988173359ab37cbbba5",
+    "url": "eikcalb.github.io/deals/static/js/2.9f47b30b.chunk.js"
+  },
+  {
+    "revision": "503e3e55662a401d0e08",
+    "url": "eikcalb.github.io/deals/static/js/main.29f8cf82.chunk.js"
+  },
+  {
+    "revision": "90e64e267f70387c184b",
+    "url": "eikcalb.github.io/deals/static/js/runtime-main.d789b624.js"
+  }
+]);
