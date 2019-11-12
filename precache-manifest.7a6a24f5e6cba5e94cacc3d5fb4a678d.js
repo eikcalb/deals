eeb04b9a63da48bde4cf97b61b7275946febfaa4
Agwa Israel Onome <agwaisraelonome@gmail.com>
2019-11-12 09:37:55 +0100

    initial

---
+++ precache-manifest.7a6a24f5e6cba5e94cacc3d5fb4a678d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c3f759c7ea0972373a7d0a57b40f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa123cb817cc530dc91",
+    "url": "/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "9e9744eb48bd92f7971c",
+    "url": "/static/css/main.b07a680c.chunk.css"
+  },
+  {
+    "revision": "1aa123cb817cc530dc91",
+    "url": "/static/js/2.9163f7b9.chunk.js"
+  },
+  {
+    "revision": "9e9744eb48bd92f7971c",
+    "url": "/static/js/main.3ab0b563.chunk.js"
+  },
+  {
+    "revision": "0fe1eecb827a9101fbc7",
+    "url": "/static/js/runtime-main.6582acc4.js"
+  }
+]);
