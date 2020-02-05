656d61d19d0da87590fe20eb1ea689906d2ba2f4
Agwa Israel Onome <agwaisraelonome@gmail.com>
2020-02-05 23:23:59 +0100

    Updates

---
+++ precache-manifest.76beeaa66f72de6409b4c14b3292fac1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b326c326e3177337ffacfbcc2fa1341e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c1f8c4aa213366423b3",
+    "url": "/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "0109e089c2058c3df5ab",
+    "url": "/static/css/main.1a6374b2.chunk.css"
+  },
+  {
+    "revision": "1c1f8c4aa213366423b3",
+    "url": "/static/js/2.7557f0fa.chunk.js"
+  },
+  {
+    "revision": "3bbbf5d57ee87cfb6aa365c57b37c52c",
+    "url": "/static/js/2.7557f0fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0109e089c2058c3df5ab",
+    "url": "/static/js/main.8cb8fc22.chunk.js"
+  },
+  {
+    "revision": "4ee7952c145135834bbc",
+    "url": "/static/js/runtime-main.83e5e061.js"
+  },
+  {
+    "revision": "724ad4b042d8789646ef1e9dfeb980c9",
+    "url": "/static/media/deal_bg.724ad4b0.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deals/precache-manifest.92cfe33724be31247901f8618c813511.js"
+  "/precache-manifest.76beeaa66f72de6409b4c14b3292fac1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deals/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
